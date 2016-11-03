9d8ccf34afa2a5d5c094bf50b5bb52505327e8d4
laijingfeng <337292616@qq.com>
2016-11-03 13:31:12 +0800

    update

--- table/excel2proto/myTable.py
+++ table/excel2proto/myTable.py
@@ -14,9 +14,18 @@ class MyTableTool(object):
     
     def __init__(self):
         pass
+
+    @classmethod
+    def to_unicode(cls, data):
+        if type(data) == str:
+            data = data.decode('utf8')
+        return data   
     
     @classmethod
     def get_use_type(cls, data):
+        if data.find('_') != -1:
+            data = data.split('_', 1)[0]
+        
         ret = MyTableTool.USE_TYPE_NONE
         if data == 'all':
             ret = MyTableTool.USE_TYPE_ALL
@@ -31,29 +40,60 @@ class MyTableTool(object):
         return ret
 
 class MyTableColumn(object):
-    def __init__(self):
-        self.idx = 0
-        self.name = ''
-        self.des = ''
-        self.type = ''
-        self.use_type = MyTableTool.TABLE_TYPE_NONE
+    def __init__(self, column_data, column_idx):
 
+        self.use_type = MyTableTool.USE_TYPE_NONE
+
+        if len(column_data) < 4:
+            return
+
+        self.use_type = MyTableTool.get_use_type(MyTableTool.to_unicode(column_data[2]))
+
+        if self.use_type == MyTableTool.USE_TYPE_NONE:
+            return
+        
+        self.idx = column_idx
+        self.type = MyTableTool.to_unicode(column_data[0])
+        self.name = MyTableTool.to_unicode(column_data[1])
+        self.des = MyTableTool.to_unicode(column_data[3])
+        
 class MyTableSheet(object):
     def __init__(self, excel_sheet, sheet_idx):
+
+        self.use_type = MyTableTool.get_use_type(excel_sheet.name)
+        if self.use_type == MyTableTool.USE_TYPE_NONE:
+            return
+        
         self.idx = sheet_idx
-        self.name = excel_sheet.name
-        self.use_type = MyTableTool.TABLE_TYPE_NONE
+        self.name = excel_sheet.name.split('_', 1)[1]
         self.colums = []
 
+        for i in range(excel_sheet.ncols):
+            column = MyTableColumn(excel_sheet.col_values(i), i)
+            if column.use_type != MyTableTool.USE_TYPE_NONE:
+                self.colums.append(column)
+                
         logger.info(sheet_idx)
+        logger.info(self.name)
+
+    def to_proto(self):
+        pass    
 
 class MyTable(object):
     def __init__(self, excel_path):
         logger.info(excel_path)
         self.name = ''
         self.sheets = []
+        
         data = xlrd.open_workbook(excel_path)
         for i in range(len(data.sheets())):
             excel_sheet = data.sheets()[i]
             sheet = MyTableSheet(excel_sheet, i)
+            if sheet.use_type != MyTableTool.USE_TYPE_NONE and len(sheet.colums) > 0:
+                self.sheets.append(sheet)
+
+    def to_proto(self):
+        logger.info('name:{}'.format(self.name))
+        #for sheet in self.sheets:
+            
             
