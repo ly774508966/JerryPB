5acde84b0ac23320aa394a7e34492c6f5037d4ec
laijingfeng <337292616@qq.com>
2016-12-23 09:32:09 +0800

    update

--- Assets/Scripts/GameApp.cs
+++ Assets/Scripts/GameApp.cs
@@ -1,28 +1,21 @@
 ﻿using UnityEngine;
+using Jerry;
 
 public class GameApp : MonoBehaviour
 {
     void Awake()
     {
+        JerryDebug.Set(true, false, false, true, typeof(ProtoBuf.ProtoMemberAttribute));
         TableLoader.Instance.LoadTables();
     }
 
     void Start()
     {
         Table.Scene scene = null;
-
+        
         if (SceneTableManager.Instance.TryGetValue(10000, out scene))
         {
-            Debug.LogError(string.Format("id={0}", scene.id));
-            Debug.LogError(string.Format("degree_type={0}", scene.degree_type));
-            Debug.LogError(string.Format("effect.type={0}", scene.effect.type));
-            Debug.LogError(string.Format("effect.val={0}", scene.effect.val));
-
-            int idx = 0;
-            foreach (uint i in scene.ulist)
-            {
-                Debug.LogError(string.Format("-{0}={1}", idx++, i));
-            }
+            JerryDebug.LogInfo(scene, true);
         }
         else
         {
