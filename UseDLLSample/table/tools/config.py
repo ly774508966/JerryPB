b017725cb78b82ef5d4edb09d8f5598bc02d9dea
laijingfeng <337292616@qq.com>
2017-04-15 12:32:15 +0800

    update

--- UseDLLSample/table/tools/config.py
+++ UseDLLSample/table/tools/config.py
@@ -8,18 +8,24 @@ class Config(object):
         self.user_type_table_path = 'UserDefineType.xlsx'
         self.table_path = '../table'
         self.proto_path = '../proto'
+
         self.client_table_prefix = 'c_table_'
         self.server_table_prefix = 's_table_'
         self.common_prefix = 'common_'
+        self.command_prefix = 'command_'
+
         self.client_table_package = 'Table'
         self.server_table_package = 'table'
-        self.common_cs_path = '../output/common_cs' # 公共CS文件存储路径
-        self.table_cs_path = '../output/table_cs' # 表格CS文件存储路径
-        self.table_data_path = '../output/table_data' # 表格CS文件存储路径
-
-        self.unity_table_cs_path = '../../Assets/Scripts/Table/proto_gen'
-        self.unity_table_data_path = '../../Assets/Resources/Table'
-        self.unity_common_cs_path = '../../Assets/Scripts/MSG/proto_gen'
+        
+        self.table_cs_path = '../output/table_cs' # 表格CS文件存储相对路径
+        self.table_data_path = '../output/table_data' # 表格二进制文件存储相对路径
+        self.common_cs_path = '../output/common_cs' # 公共PB的CS文件存储相对路径
+        self.command_cs_path = '../output/command_cs' # 协议PB的CS文件存储相对路径
+        
+        self.unity_table_cs_path = '../../Assets/Scripts/Table/proto_gen' # Unity中表格CS文件存储相对路径
+        self.unity_table_data_path = '../../Assets/Resources/Table' # Unity中表格二进制文件存储相对路径
+        self.unity_common_cs_path = '../../Assets/Scripts/MSG/proto_gen' # Unity中公共PB的CS文件存储相对路径
+        self.unity_command_cs_path = '../../Assets/Scripts/MSG/cmd' # Unity中协议PB的CS文件存储相对路径
 
     @classmethod
     def proto_to_cs(cls, proto_name, cs_path):
@@ -40,4 +46,3 @@ class Config(object):
             with open(cs_path,'w') as f:
                 text = text.replace(pattern, target)
                 f.write(text)
-        
