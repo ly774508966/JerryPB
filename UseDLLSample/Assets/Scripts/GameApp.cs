a23d99df63ebf13fbc949c0f8fb3428e12c7a3c1
laijingfeng <337292616@qq.com>
2017-04-15 17:21:00 +0800

    update

--- UseDLLSample/Assets/Scripts/GameApp.cs
+++ UseDLLSample/Assets/Scripts/GameApp.cs
@@ -1,29 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Jerry;
-using System;
 
 public class GameApp : MonoBehaviour
 {
-#if TableFromAB
-    private bool _isJABOK = false;
-#endif
     private bool _isTableOK = false;
 
     void Awake()
     {
-        JerryDebug.Inst.Set(true, false, false, true, false, typeof(ProtoBuf.ProtoMemberAttribute));
+        JerryDebug.Inst.Set(true, false, false, true, false);
 
 #if TableFromAB
-        this.StartCoroutine(InitializeJAB(() =>
+        JABMgr.Init((success) =>
         {
-            _isJABOK = true;
-
-            this.StartCoroutine(MyTableLoader.Inst.LoadTables(() =>
+            if (success)
             {
-                _isTableOK = true;
-            }));
-        }));
+                this.StartCoroutine(MyTableLoader.Inst.LoadTables(() =>
+                {
+                    _isTableOK = true;
+                }));
+            }
+        }, JABUtil.Platform.Android);
 #else
         this.StartCoroutine(MyTableLoader.Inst.LoadTables(() =>
         {
@@ -39,9 +36,6 @@ public class GameApp : MonoBehaviour
 
     private IEnumerator WaitResOK()
     {
-#if TableFromAB
-        yield return new WaitUntil(() => this._isJABOK == true);
-#endif
         yield return new WaitUntil(() => this._isTableOK == true);
         GameStart();
     }
@@ -68,22 +62,4 @@ public class GameApp : MonoBehaviour
             Debug.LogError("not exist");
         }
     }
-
-#if TableFromAB
-    private IEnumerator InitializeJAB(Action callback = null)
-    {
-        JABMgr.Set(JABUtil.JPlatformName.Android);
-
-        JABLoadManifestOperation request = JABMgr.LoadManifest();
-        if (request != null)
-        {
-            yield return this.StartCoroutine(request);
-        }
-
-        if (callback != null)
-        {
-            callback();
-        }
-    }
-#endif
 }
